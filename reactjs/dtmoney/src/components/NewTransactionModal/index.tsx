0aa72d253e3eff80153c1fa3ef0fe10dda9c67ef
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2021-09-03 16:27:24 -0300

    feat: application finalized

--- reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
+++ reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,11 @@
 import { FormEvent, useState } from 'react'
 import Modal from 'react-modal'
+
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
+import { useTransactions } from '../../hooks/useTransactions'
+
 import { Container, RadioBox, TransactionTypeContainer } from './styles'
 
 interface NewTransactionModalProps {
@@ -16,11 +19,25 @@ export function NewTransactionModal({
 }: NewTransactionModalProps) {
   const [title, setTitle] = useState('')
   const [category, setCategory] = useState('')
-  const [value, setValue] = useState(0)
+  const [amount, setAmount] = useState(0)
   const [type, setType] = useState('deposit')
+  const { createTransaction } = useTransactions()
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault()
+
+    await createTransaction({
+      title,
+      amount,
+      category,
+      type,
+    })
+
+    setTitle('')
+    setAmount(0)
+    setCategory('')
+    setType('deposit')
+    onRequestClose()
   }
 
   return (
@@ -41,8 +58,17 @@ export function NewTransactionModal({
       <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar transação</h2>
 
-        <input placeholder='Título' value={title} />
-        <input placeholder='Valor' type='number' value={value} />
+        <input
+          placeholder='Título'
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+        />
+        <input
+          placeholder='Valor'
+          type='number'
+          value={amount}
+          onChange={event => setAmount(Number(event.target.value))}
+        />
 
         <TransactionTypeContainer>
           <RadioBox
@@ -69,7 +95,11 @@ export function NewTransactionModal({
           </RadioBox>
         </TransactionTypeContainer>
 
-        <input placeholder='Categoria' value={category} />
+        <input
+          placeholder='Categoria'
+          value={category}
+          onChange={event => setCategory(event.target.value)}
+        />
 
         <button type='submit'>Cadastrar</button>
       </Container>
--- reactjs/dtmoney/src/components/TransactionsTable/index.tsx
+++ reactjs/dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,11 +1,8 @@
-import { useEffect } from 'react'
-import { api } from '../../services/api'
+import { useTransactions } from '../../hooks/useTransactions'
 import { Container } from './styles'
 
 export function TransactionTable() {
-  useEffect(() => {
-    api.get('/transactions').then(response => console.log(response.data))
-  }, [])
+  const { transactions } = useTransactions()
 
   return (
     <Container>
@@ -20,18 +17,23 @@ export function TransactionTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de Website</td>
-            <td className='deposit'>R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
-          <tr>
-            <td>Aluguel</td>
-            <td className='withdraw'>- R$1.100</td>
-            <td>Casa</td>
-            <td>15/02/2021</td>
-          </tr>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('pt-br', {
+                  style: 'currency',
+                  currency: 'BRL',
+                }).format(transaction.amount)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {new Intl.DateTimeFormat('pt-br').format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Container>
