9855713df6cb44e90a0155514d01cbe0f3518b7d
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2021-09-03 12:01:21 -0300

    feat: working with modal and forms

--- reactjs/dtmoney/src/styles/global.ts
+++ reactjs/dtmoney/src/styles/global.ts
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
   :root {
-    --background: #f2f0f5;
+    --background: #f0f2f5;
     --shape: #ffffff;
 
     --green: #33cc95;
@@ -32,7 +32,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body { 
-    background: var(---background);
+    background: var(--background);
     -webkit-font-smoothing: antialiased;
   }
 
@@ -52,5 +52,42 @@ export const GlobalStyle = createGlobalStyle`
   [disabled] {
     opacity: 0.6;
     cursor: not-allowed
+  }
+
+  .react-modal-overlay {
+    background: rgba(0,0,0,0.5);
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content {
+    width: 100%;
+    max-width: 576px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.24rem;
+  }
+
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.8)
+    }
   }
 `
