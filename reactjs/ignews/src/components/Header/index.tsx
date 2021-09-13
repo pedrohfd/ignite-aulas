3048430067dff128c91b731a9223916b480e7e6f
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2021-09-13 13:56:41 -0300

    feat: creating post page

--- reactjs/ignews/src/components/Header/index.tsx
+++ reactjs/ignews/src/components/Header/index.tsx
@@ -1,17 +1,23 @@
+import { useRouter } from 'next/router'
+import Link from 'next/dist/client/link'
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
+import { ActiveLink } from '../ActiveLink'
 
 export function Header() {
+  const { asPath } = useRouter()
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src='/images/logo.svg' alt='ig.news' />
 
         <nav>
-          <a href='' className={styles.active}>
-            Home
-          </a>
-          <a href=''>Posts</a>
+          <ActiveLink activeClassName={styles.active} href='/'>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href='/posts'>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
